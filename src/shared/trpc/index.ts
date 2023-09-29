da61cd2324c0a5770a333c3e316712fcea19d922
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-29 17:19:47 -0500

    [ADD]: appRouter trpc (authCallback method)

--- src/shared/trpc/index.ts
+++ src/shared/trpc/index.ts
@@ -1,10 +1,34 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
 import { publicProcedure, router } from './trpc';
+import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
 
 // router of backend side with safe typing
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
-    // const {}
-    return 2;
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user?.id || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+    // // check if the user is in the DB
+    const dbUser = await db.user.findFirst({
+      where: { id: user.id },
+    });
+    if (!dbUser) {
+      // is the 1st time, we need to create user in our db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+
+    return {
+      success: true,
+    };
   }),
 });
 
