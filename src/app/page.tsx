f23c95bbde0ed2241e210c0ba029431f5a953a74
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-29 09:36:55 -0500

    [ADD]: LandingDecorationSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,41 @@
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 
+import { buttonVariants } from '@/components/ui/button';
 import { MaxWithWrapper } from './components/MaxWithWrapper';
+import { LandingDecorationSection } from './shared/components';
 
 export default function Home() {
   return (
-    <MaxWithWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-        <p className="text-sm font-semibold text-gray-700">
-          Quill is now public!
+    <>
+      <MaxWithWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">
+            Quill is now public!
+          </p>
+        </div>
+
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Chat with your <span className="text-blue-600">documents</span> in
+          seconds.
+        </h1>
+
+        <p className="mt-5 max-w-prose text-zinc-600 sm:text-lg">
+          Quill allows you to have conversations with any PDF document. Simply
+          upload your file and start asking questions right away
         </p>
-      </div>
 
-      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-        Chat with your <span className="text-blue-600">documents</span> in
-        seconds.
-      </h1>
+        <Link
+          className={buttonVariants({ size: 'lg', className: 'mt-5' })}
+          href="/dashboard"
+          target="_blank"
+        >
+          Get started <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </MaxWithWrapper>
 
-      <p className="mt-5 max-w-prose text-zinc-600 sm:text-lg">
-        Quill allows you to have conversations with any PDF document. Simply
-        upload your file and start asking questions right away
-      </p>
-
-      <Link href="/dashboard" target="_blank">
-        Get started <ArrowRight className="ml-2 h-5 w-5" />
-      </Link>
-    </MaxWithWrapper>
+      {/* value position section: only for decoration,so avoid read it with aria-hidden */}
+      <LandingDecorationSection />
+    </>
   );
 }
---
+++ src/app/shared/components/LandingDecorationSection/index.ts
@@ -0,0 +1,1 @@
+export { default as LandingDecorationSection } from './LandingDecorationSection';
