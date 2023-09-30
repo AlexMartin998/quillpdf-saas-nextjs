692be30ea55d4bc4ceb4939bbc850137c9284c8d
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-29 23:34:24 -0500

    [ADD]: Skeleton dep + component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Navbar } from '@/shared/components';
 
+import 'react-loading-skeleton/dist/skeleton.css';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
