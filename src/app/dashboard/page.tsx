2104e4e7cbcd196d97722625773f30895b00d5e3
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-29 20:44:02 -0500

    [ADD]: validate if user is already created in dashboad server component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,16 +2,24 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextPage } from 'next';
 import { redirect } from 'next/navigation';
 
+import { db } from '@/db';
+import { DashboardScene } from '@/quillpdf/scenes';
+
 export type DashboardPageProps = {};
 
-const DashboardPage: NextPage<DashboardPageProps> = () => {
+// async is allawed 'cause in v13+ this is a ServerComponent
+const DashboardPage: NextPage<DashboardPageProps> = async () => {
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
   // the 1st time user does noe exist in our db, so it sync db to allow set auth
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
 
-  return <>Hello</>;
+  // check if the user is already registered in our db
+  const dbUser = await db.user.findFirst({ where: { id: user.id } });
+  if (!dbUser) redirect('/auth-callback?origin=dashboard');
+
+  return <DashboardScene />;
 };
 
 export default DashboardPage;
