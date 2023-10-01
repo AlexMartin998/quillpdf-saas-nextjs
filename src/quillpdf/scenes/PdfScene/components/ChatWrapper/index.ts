2e2b02f0c05d7d3b7d675f30e0a87ecb830a70ec
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-01 15:25:21 -0500

    [ADD]: ChatInput design

--- src/quillpdf/scenes/PdfScene/components/ChatWrapper/index.ts
+++ src/quillpdf/scenes/PdfScene/components/ChatWrapper/index.ts
@@ -1,2 +1,5 @@
+export { default as ChatInput } from './ChatInput/ChatInput';
 export { default as ChatWrapper } from './ChatWrapper';
 export { default as ChatWrapperLoader } from './ChatWrapperLoader/ChatWrapperLoader';
+export { default as InvalidPDFProcessingMsg } from './InvalidPDFProcessingMsg/InvalidPDFProcessingMsg';
+export { default as Messages } from './Messages/Messages';
