5d75fb7afcdbd0e45c64967df9c769f39e37f661
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-30 22:23:47 -0500

    [ADD]: pdf input page functionality

--- src/quillpdf/scenes/PdfScene/components/PdfRenderer/PdfRenderer.tsx
+++ src/quillpdf/scenes/PdfScene/components/PdfRenderer/PdfRenderer.tsx
@@ -1,14 +1,19 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
 import { useResizeDetector } from 'react-resize-detector';
+import { z } from 'zod';
+
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
 import { Button } from '@/shared/components/ui/button';
 import { Input } from '@/shared/components/ui/input';
 import { useToast } from '@/shared/components/ui/use-toast';
+import { cn } from '@/shared/lib/utils';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -19,7 +24,28 @@ const PdfRenderer: React.FC<PdfRendererProps> = ({ url }) => {
   const { width, ref } = useResizeDetector();
 
   const [numPages, setNumPages] = useState<number>();
-  const [currentPage, setCurrentPage] = useState<number>(3);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  // form & input page validator
+  const CustomPageValidator = z.object({
+    page: z.string().refine(num => +num > 0 && +num <= numPages!),
+  });
+  type TCustomPageValidator = z.infer<typeof CustomPageValidator>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<TCustomPageValidator>({
+    defaultValues: { page: '1' },
+    resolver: zodResolver(CustomPageValidator),
+  });
+
+  const handlePageSubmit = ({ page }: TCustomPageValidator) => {
+    setCurrentPage(+page);
+    setValue('page', page.toString());
+  };
 
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
@@ -27,7 +53,12 @@ const PdfRenderer: React.FC<PdfRendererProps> = ({ url }) => {
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         <div className="flex items-center gap-1.5">
           <Button
-            onClick={() => currentPage > 1 && setCurrentPage(prev => prev - 1)}
+            onClick={() => {
+              if (currentPage > 1) {
+                setCurrentPage(prev => prev - 1);
+                setValue('page', String(currentPage - 1));
+              }
+            }}
             disabled={currentPage <= 1}
             variant="ghost"
             aria-label="previous page"
@@ -37,7 +68,18 @@ const PdfRenderer: React.FC<PdfRendererProps> = ({ url }) => {
 
           {/* navigate */}
           <div className="flex items-center gap-1.5">
-            <Input className="w-12 h-8" />
+            <Input
+              {...register('page')}
+              className={cn(
+                'w-12 h-8',
+                errors.page && 'focus-visible:ring-red-500'
+              )}
+              onKeyUp={e => {
+                if (e.key === 'Enter') {
+                  handleSubmit(handlePageSubmit)();
+                }
+              }}
+            />
             <p className="text-zinc-700 text-sm space-x-1">
               <span>/</span>
               <span>{numPages ?? 'x'}</span>
@@ -45,9 +87,12 @@ const PdfRenderer: React.FC<PdfRendererProps> = ({ url }) => {
           </div>
 
           <Button
-            onClick={() =>
-              currentPage < numPages! && setCurrentPage(prev => prev + 1)
-            }
+            onClick={() => {
+              if (currentPage < numPages!) {
+                setCurrentPage(prev => prev + 1);
+                setValue('page', String(currentPage + 1));
+              }
+            }}
             disabled={currentPage >= numPages! || currentPage === undefined}
             variant="ghost"
             aria-label="next page"
