b49b17e5a602fbfd6a6d241317c57218385df09c
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-01 19:49:30 -0500

    [ADD]: vectorize and index entire document

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,12 @@
-import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
+
+import { db } from '@/db';
+import { PineconeConstants } from '@/shared/constants';
+import { pinecone } from '@/shared/lib/pinecone';
 
 const f = createUploadthing();
 
@@ -27,6 +33,50 @@ export const ourFileRouter = {
           uploadStatus: 'PROCESSING',
         },
       });
+
+      // // // indexing file for AI propouses
+      try {
+        const res = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+        const blob = await res.blob();
+
+        //
+        const loader = new PDFLoader(blob);
+        const pageLevelDocs = await loader.load(); // pdf content
+        const pagesAmt = pageLevelDocs.length; // to check subscription (pro/free plan)
+
+        // // vectorize and index entire document
+        const pineconeIndex = pinecone.Index(PineconeConstants.IndexName);
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        }); // reuse generated vector from text (pdf content)
+
+        // save vector
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          // @ts-ignore
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+
+        await db.file.update({
+          data: {
+            uploadStatus: 'SUCCESS',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        await db.file.update({
+          data: {
+            uploadStatus: 'FAILED',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
