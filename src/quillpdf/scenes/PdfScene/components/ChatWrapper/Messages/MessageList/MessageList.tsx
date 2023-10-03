33ed06284d110d5be8020e47f89377cb02f8ca54
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-03 13:58:35 -0500

    [ADD]: set ref to last message (limit) to start another fetching data as a infinite query

--- src/quillpdf/scenes/PdfScene/components/ChatWrapper/Messages/MessageList/MessageList.tsx
+++ src/quillpdf/scenes/PdfScene/components/ChatWrapper/Messages/MessageList/MessageList.tsx
@@ -1,9 +1,33 @@
+'use client';
+
+import { useIntersection } from '@mantine/hooks';
+import { useEffect, useRef } from 'react';
+
 import { CombinedMessages } from '@/shared/types';
 import { Message } from '..';
 
-export type MessageListProps = { combinedMessages: CombinedMessages };
+export type MessageListProps = {
+  combinedMessages: CombinedMessages;
+  fetchNextPage: any;
+};
 
-const MessageList: React.FC<MessageListProps> = ({ combinedMessages }) => {
+const MessageList: React.FC<MessageListProps> = ({
+  combinedMessages,
+  fetchNextPage,
+}) => {
+  const lastMessageRef = useRef<HTMLDivElement>(null);
+
+  const { ref, entry } = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
+
   return (
     <>
       {combinedMessages.map((message, i) => {
@@ -14,6 +38,7 @@ const MessageList: React.FC<MessageListProps> = ({ combinedMessages }) => {
         if (i === combinedMessages.length - 1)
           return (
             <Message
+              ref={ref}
               message={message}
               isNextMessageSamePerson={isNextMessageSamePerson}
               key={message.id}
@@ -22,6 +47,7 @@ const MessageList: React.FC<MessageListProps> = ({ combinedMessages }) => {
         else
           return (
             <Message
+              ref={ref}
               message={message}
               isNextMessageSamePerson={isNextMessageSamePerson}
               key={message.id}
--- src/quillpdf/scenes/PdfScene/components/ChatWrapper/Messages/Messages.tsx
+++ src/quillpdf/scenes/PdfScene/components/ChatWrapper/Messages/Messages.tsx
@@ -48,7 +48,10 @@ const Messages: React.FC<MessagesProps> = ({ fileId }) => {
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {!!combinedMessages?.length ? (
-        <MessageList combinedMessages={combinedMessages} />
+        <MessageList
+          combinedMessages={combinedMessages}
+          fetchNextPage={fetchNextPage}
+        />
       ) : isLoading ? (
         <div className="w-full flex flex-col gap-2">
           <Skeleton className="h-16" />
