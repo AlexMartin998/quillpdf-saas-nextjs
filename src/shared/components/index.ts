c05a1bb363af21eb5812730a867b97320443cbe1
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-04 10:11:46 -0500

    [ADD]: UserAccountNav

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,4 @@
-export { default as MaxWidthWrapper } from './MaxWidthWrapper/MaxWidthWrapper';
-export * from './ui';
 export { default as BillingForm } from './BillingForm/BillingForm';
+export { default as MaxWidthWrapper } from './MaxWidthWrapper/MaxWidthWrapper';
+export { default as UserAccountNav } from './UserAccountNav/UserAccountNav';
+export * from './ui';
