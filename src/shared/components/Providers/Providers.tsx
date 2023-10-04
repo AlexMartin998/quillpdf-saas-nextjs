5ebc4498663a142e5e46eab79f5049a75f2c7627
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-04 10:41:11 -0500

    [UPD]: tPRC Provider to use correct url

--- src/shared/components/Providers/Providers.tsx
+++ src/shared/components/Providers/Providers.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { trpc } from '@/app/_trpc/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { useState } from 'react';
 
-// export type ProvidersProps = { children: React.ReactNode };
+import { trpc } from '@/app/_trpc/client';
+import { absoluteUrl } from '@/shared/lib/utils';
 
 const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      links: [httpBatchLink({ url: 'http://localhost:3000/api/trpc' })],
+      links: [httpBatchLink({ url: absoluteUrl('/api/trpc') })],
     })
   );
 
