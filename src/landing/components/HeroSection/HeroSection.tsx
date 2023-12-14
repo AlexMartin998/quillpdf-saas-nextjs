f86b1e3339b07bf52efcefe9755470a994838790
Adrian <alextravc.342@gmail.com>
2023-12-14 15:27:05 -0500

    [ADD]: get started link

--- src/landing/components/HeroSection/HeroSection.tsx
+++ src/landing/components/HeroSection/HeroSection.tsx
@@ -1,10 +1,14 @@
 import { buttonVariants } from '@/shared/components/ui/button';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 
 export type HeroSectionProps = {};
 
 const HeroSection: React.FC<HeroSectionProps> = () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
   return (
     <section>
       <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
@@ -24,8 +28,11 @@ const HeroSection: React.FC<HeroSectionProps> = () => {
       </p>
 
       <Link
-        className={buttonVariants({ size: 'lg', className: 'mt-5' })}
-        href="/dashboard"
+        className={buttonVariants({
+          size: 'lg',
+          className: 'mt-5',
+        })}
+        href={user ? '/dashboard' : '/api/auth/register'}
         target="_blank"
       >
         Get started <ArrowRight className="ml-2 h-5 w-5" />
